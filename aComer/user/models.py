72635354955406d162646bd728454c39e63f42f8
Carlos <carlos@m1.local>
2021-04-02 09:37:26 -0600

    customer models charlie

--- aComer/user/models.py
+++ aComer/user/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    """ Customer information """
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=25)
+    email = models.EmailField(unique=True)
+
+    def __str__(self) -> str:
+        return f"{self.first_name, self.last_name}"
+
+class CustomerAddrress(models.Model):
+    """ Customer address """
+    alias = models.CharField(max_length=50)
+    street = models.CharField(max_length=50)
+    exterior_no = models.CharField(max_length=10)
+    interior_no = models.CharField(max_length=10)
+    neighborhood = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    municipality = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f"{self.alias}"
+
+class Review (models.Model):
+    """ Customer address """
+    title = models.CharField(max_length=50)
+    review = models.CharField(max_length=500)
+
+    def __str__(self) -> str:
+        return f"{self.title}"
+
