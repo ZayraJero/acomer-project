e275fb7ffd0dd6ef8b2009539f084256648604e6
msalda <msalda53@gmail.com>
2021-04-02 10:37:30 -0600

    add user models and realtions

--- aComer/fonda/models.py
+++ aComer/fonda/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from aComer.user import models as umodels
 
 # Create your models fonda here. 
 
@@ -11,11 +12,7 @@ class Restaurant(models.Model):
     def __str__(self) -> str:
         return f"{self.rest_name}"
 
-class Client(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    phone = models.CharField(max_length=25)
-    email = models.EmailField(unique=True)
+
 
 
 class Item(models.Model):
@@ -23,17 +20,6 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
 
 
-class ClientAddress(models.Model):
-    alias = models.CharField(max_length=50)
-    street = models.CharField(max_length=75)
-    suburb = models.CharField(max_length=100)
-    municipality = models.CharField (max_length=50)
-    state = models.CharField(max_length=50)
-    int_number = models.IntegerField(max_length=10)
-    ext_number = models.IntegerField(max_length=10)
-
-    #Relations
-    client = models.ForeignKey(Client,on_delete=models.PROTECT,related_name="clients")
 
 class Order(models.Model):
     STATUS_TYPES = (
@@ -46,22 +32,10 @@ class Order(models.Model):
    
     #Relations
     restaurant = models.ForeignKey(Restaurant, on_delete=models.PROTECT, related_name="restaurant_addresses")
-    client = models.ForeignKey(Client,on_delete=models.PROTECT,related_name="clients")
+    client = models.ForeignKey(umodels.Client,on_delete=models.PROTECT,related_name="clients")
     item = models.ForeignKey(Item,on_delete=models.PROTECT,related_name="items")
 
-class Ratings(models.Model):
-    RATING_TYPES = (
-        ("pesimo","Pesimo"),
-        ("malo","Malo"),
-        ("regular","Regular"),
-        ("bueno","Bueno"),
-        ("muy bueno","Muy Bueno"),
-    )
-    rating = models.CharField(max_length=50,choices=RATING_TYPES,default="muy bueno")
 
-    #Relations
-    restaurant = models.ForeignKey(Restaurant, on_delete=models.PROTECT, related_name="restaurant_addresses")
-    client = models.ForeignKey(Client,on_delete=models.PROTECT,related_name="clients")
 
 
 class RastaurantAddress(models.Model):
@@ -81,7 +55,7 @@ class RastaurantAddress(models.Model):
 class Menu(models.Model):
     """ menu """
     entrance = models.CharField(max_length=150)
-    first_time = models,CharField(max_length=150)
+    first_time = models.CharField(max_length=150)
     main_course = models.CharField(max_length=150)
     dessert = models.CharField(max_length=150)
     drink = models.CharField(max_length=150)
--- aComer/user/models.py
+++ aComer/user/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from aComer.fonda import models as fmodels
 
 # Create your models here.
+class Client(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=25)
+    email = models.EmailField(unique=True)
+
+class ClientAddress(models.Model):
+    alias = models.CharField(max_length=50)
+    street = models.CharField(max_length=75)
+    suburb = models.CharField(max_length=100)
+    municipality = models.CharField (max_length=50)
+    state = models.CharField(max_length=50)
+    int_number = models.IntegerField(max_length=10)
+    ext_number = models.IntegerField(max_length=10)
+    #Relations
+    client = models.ForeignKey(Client,on_delete=models.PROTECT,related_name="clients")
+
+class Ratings(models.Model):
+    RATING_TYPES = (
+        ("pesimo","Pesimo"),
+        ("malo","Malo"),
+        ("regular","Regular"),
+        ("bueno","Bueno"),
+        ("muy bueno","Muy Bueno"),
+    )
+
+    rating = models.CharField(max_length=50,choices=RATING_TYPES,default="muy bueno")
+
+    #Relations
+    restaurant = models.ForeignKey(fmodels.Restaurant, on_delete=models.PROTECT, related_name="restaurant_addresses")
+    client = models.ForeignKey(Client,on_delete=models.PROTECT,related_name="clients")
