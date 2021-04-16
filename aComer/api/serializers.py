ad5731dfd9d0fec1d82ab6ec2c6272e1629501a1
msalda <msalda53@gmail.com>
2021-04-15 19:12:39 -0500

    add generic views

---
+++ aComer/api/serializers.py
@@ -0,0 +1,186 @@
+from django.db.models import fields
+from rest_framework import serializers
+from fonda.models import (
+    Menu,
+    Order,
+    Plate,
+    Restaurant,
+    RestaurantAddress,
+    )
+from user.models import (
+    Client,
+    ClientAddress,
+    Rating
+)
+#Restaurants
+class RestaurantListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Restaurant
+        fields = [
+            "id",
+            "name",
+            "email",
+            "phone",
+            ]
+        
+
+class RestaurantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Restaurant
+        fields = [
+            "id",
+            "name",
+            ]
+
+
+
+
+#Plates
+
+
+
+class PlateListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Plate
+        fields = [
+            "id",
+            "type",
+            "name",
+            "price",
+            ]
+
+class PlateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Plate
+        fields = [
+            "type",
+            "name",
+            ]
+
+
+#Orders
+class OrderListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = [
+            "status",
+            ]
+
+
+
+#restaurantAddresses
+class RestaurantAddressesListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RestaurantAddress
+        fields = [
+            "status",
+            "street",
+            "suburb",
+            "municipality",
+            "state",
+            "int_number",
+            "ext_number",
+            "zip_code",
+            ]
+
+class RestaurantAddressesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RestaurantAddress
+        fields = [
+            "street",
+            "suburb",
+            "municipality",
+            "int_number",
+            "ext_number",
+            ]
+
+#foreign restaurant
+class RestaurantOrdersSerializer(serializers.ModelSerializer):
+    class Meta:
+        street = RestaurantAddressesSerializer()
+        model = Restaurant
+        fields = [
+            "id",
+            "name",
+            "email",
+            "phone",
+            "street",
+            ]
+#Menu
+
+class MenusListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Menu
+        fields = [
+            "title",
+            "description",
+            "groupMenu",
+            "price",
+            ]
+
+
+class MenusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Menu
+        fields = [
+            "title",
+            "description",
+            ]
+
+#Client
+
+class ClientsListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Client
+        fields = [
+            "id",
+            "first_name",
+            "last_name",
+            "phone",
+            "email",
+            ]
+
+class ClientsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Client
+        fields = [
+            "id",
+            "first_name",
+            "last_name",
+            ]
+
+#client address
+
+class ClientAddressesListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ClientAddress
+        fields = [
+            "id",
+            "alias",
+            "suburb",
+            "municipality",
+            "state",
+            "int_number",
+            "ext_number",
+            ]
+
+class ClientAddressesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ClientAddress
+        fields = [
+            "alias",
+            "suburb",
+            "municipality",
+            "int_number",
+            "ext_number",
+            ]
+
+#rating
+
+class RaitingsListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rating
+        fields = [
+            "rating"
+            ]
+
---
+++ aComer/api/urls.py
@@ -0,0 +1,108 @@
+from django.db import router
+from django.urls import path,include
+from rest_framework import routers
+from .views import (
+    #Restaurant
+    ListRestaurantsAPIView,
+    CreateRestaurantAPIView,
+    RetrieveRestaurantAPIView,
+    UpdateRestaurantAPIView,
+    DeleteRestaurantAPIView,
+    RetrieveRestaurantOrdersAPIView,
+    #Plate
+    ListPlatesAPIView,
+    CreatePlatesAPIView,
+    RetrievePlatesAPIView,
+    UpdatePlatesAPIView,
+    DeletePlatesAPIView,
+    #order
+    ListOrdersAPIView,
+    CreateOrdersAPIView,
+    RetrieveOrdersAPIView,
+    UpdateOrdersAPIView,
+    DeleteOrdersAPIView,
+    #restaurant addreses
+    ListRestaurantAddressesAPIView,
+    CreateRestaurantAddressesAPIView,
+    RetrieveRestaurantAddressesAPIView,
+    UpdateRestaurantAddressesAPIView,
+    DeleteRestaurantAddressesAPIView,
+    #Menu
+    ListMenusAPIView,
+    CreateMenusAPIView,
+    RetrieveMenusAPIView,
+    UpdateMenusAPIView,
+    DeleteMenusAPIView,
+    #client
+    ListClientsAPIView,
+    CreateClientsAPIView,
+    RetrieveClientsAPIView,
+    UpdateClientsAPIView,
+    DeleteClientsAPIView,
+    #client addresses
+    ListAddressesAPIView,
+    CreateAddressesAPIView,
+    RetrieveAddressesAPIView,
+    UpdateAddressesAPIView,
+    DeleteAddressesAPIView,
+    #raitings
+    ListRatingsAPIView,
+    CreateRatingsAPIView,
+    RetrieveRatingsAPIView,
+    UpdateRatingsAPIView,
+    DeleteRatingsAPIView,
+    )
+
+urlpatterns=[
+    #Restaurant
+    path("restaurant/",ListRestaurantsAPIView.as_view(),name="list-restaurants"),
+    path("restaurant/create/",CreateRestaurantAPIView.as_view(),name="create-restaurants"),
+    path("restaurant/<int:pk>/detail/",RetrieveRestaurantAPIView.as_view(),name="detail-restaurants"),
+    path("restaurant/<int:pk>/update/",UpdateRestaurantAPIView.as_view(),name="update-restaurants"),
+    path("restaurant/<int:pk>/delete/",DeleteRestaurantAPIView.as_view(),name="delete-restaurants"),
+    path("restaurant/<int:pk>/order/",RetrieveRestaurantOrdersAPIView.as_view(),name="detail-restaurantsOrder"),
+    #plate
+    path("plate/",ListPlatesAPIView.as_view(),name="list-plate"),
+    path("plate/create/",CreatePlatesAPIView.as_view(),name="create-plate"),
+    path("plate/<int:pk>/detail/",RetrievePlatesAPIView.as_view(),name="detail-plate"),
+    path("plate/<int:pk>/update/",UpdatePlatesAPIView.as_view(),name="update-plate"),
+    path("plate/<int:pk>/delete/",DeletePlatesAPIView.as_view(),name="delete-plate"),
+    #order
+    path("order/",ListOrdersAPIView.as_view(),name="list-plate"),
+    path("order/create/",CreateOrdersAPIView.as_view(),name="create-plate"),
+    path("order/<int:pk>/detail/",RetrieveOrdersAPIView.as_view(),name="detail-plate"),
+    path("order/<int:pk>/update/",UpdateOrdersAPIView.as_view(),name="update-plate"),
+    path("order/<int:pk>/delete/",DeleteOrdersAPIView.as_view(),name="delete-plate"),
+    #restaurantaddres
+    path("raddress/",ListRestaurantAddressesAPIView.as_view(),name="list-raddress"),
+    path("raddress/create/",CreateRestaurantAddressesAPIView.as_view(),name="create-raddress"),
+    path("raddress/<int:pk>/detail/",RetrieveRestaurantAddressesAPIView.as_view(),name="detail-raddress"),
+    path("raddress/<int:pk>/update/",UpdateRestaurantAddressesAPIView.as_view(),name="update-raddress"),
+    path("raddress/<int:pk>/delete/",DeleteRestaurantAddressesAPIView.as_view(),name="delete-raddress"),
+    #Menu
+    path("menu/",ListMenusAPIView.as_view(),name="list-menu"),
+    path("menu/create/",CreateMenusAPIView.as_view(),name="create-menu"),
+    path("menu/<int:pk>/detail/",RetrieveMenusAPIView.as_view(),name="detail-menu"),
+    path("menu/<int:pk>/update/",UpdateMenusAPIView.as_view(),name="update-menu"),
+    path("menu/<int:pk>/delete/",DeleteMenusAPIView.as_view(),name="delete-menu"),
+    #client
+    path("client/",ListClientsAPIView.as_view(),name="list-client"),
+    path("client/create/",CreateClientsAPIView.as_view(),name="create-client"),
+    path("client/<int:pk>/detail/",RetrieveClientsAPIView.as_view(),name="detail-client"),
+    path("client/<int:pk>/update/",UpdateClientsAPIView.as_view(),name="update-client"),
+    path("client/<int:pk>/delete/",DeleteClientsAPIView.as_view(),name="delete-client"),
+    #client address
+    path("address/",ListAddressesAPIView.as_view(),name="list-address"),
+    path("address/create/",CreateAddressesAPIView.as_view(),name="create-address"),
+    path("address/<int:pk>/detail/",RetrieveAddressesAPIView.as_view(),name="detail-address"),
+    path("address/<int:pk>/update/",UpdateAddressesAPIView.as_view(),name="update-address"),
+    path("address/<int:pk>/delete/",DeleteAddressesAPIView.as_view(),name="delete-address"),
+    #ratings
+    path("rating/",ListRatingsAPIView.as_view(),name="list-rating"),
+    path("rating/create/",CreateRatingsAPIView.as_view(),name="create-rating"),
+    path("rating/<int:pk>/detail/",RetrieveRatingsAPIView.as_view(),name="detail-rating"),
+    path("rating/<int:pk>/update/",UpdateRatingsAPIView.as_view(),name="update-rating"),
+    path("rating/<int:pk>/delete/",DeleteRatingsAPIView.as_view(),name="delete-rating"),
+
+
+]
